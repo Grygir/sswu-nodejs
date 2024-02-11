3bc9b264b8ffe43a40143a6c9d3b52840a3d2a53
Hryhorii Hrebiniuk <grebinuk@gmail.com>
2024-02-11 10:15:20 +0200

    homework-16

--- homework-16/src/utils/jwt.utils.js
+++ homework-16/src/utils/jwt.utils.js
@@ -1,50 +1,16 @@
 import jwt from 'jsonwebtoken';
 import serverConfig from '../config/server.config.js';
-import models from "../models/index.js";
 import * as crypto from 'crypto';
-
-const {JWT} = models;
+import * as jwtsService from '../services/jwts.service.js';
 
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } =  serverConfig;
 
-const saveJWT = async (token, type) => {
-    const {jti, exp, id} = jwt.decode(token);
-    if (jti) {
-        await JWT.create({
-            userId: id,
-            jwtid: jti,
-            type: type,
-            expireAt: new Date(exp * 1000).toISOString()
-        });
-    }
-};
-
-const revokeJWT = async (token) => {
-    const {jti} = jwt.decode(token);
-    if (jti) {
-        return JWT.destroy({ where: { jwtid: jti } });
-    }
-};
-
-const isAvailableJWT = async (token) => {
-    const {jti} = jwt.decode(token);
-    if (jti) {
-        return JWT.findOne({ where: { jwtid: jti }, attributes: ['jwtid'] });
-    } else {
-        return true;
-    }
-}
-
 export const createAccessToken = async (payload, options = {}) => {
-    const token = jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '1h', ...options });
-    await saveJWT(token, 'access');
-    return token;
+    return jwt.sign(payload, ACCESS_TOKEN_SECRET, { expiresIn: '1h', ...options });
 }
 
 export const createRefreshToken = async (payload, options = {}) => {
-    const token = jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '180d', ...options });
-    await saveJWT(token, 'refresh');
-    return token;
+    return jwt.sign(payload, REFRESH_TOKEN_SECRET, { expiresIn: '180d', ...options });
 }
 
 export const createTokens = async (payload) => {
@@ -53,6 +19,10 @@ export const createTokens = async (payload) => {
         createAccessToken(payload, { jwtid }),
         createRefreshToken(payload, { jwtid })
     ]);
+    await jwtsService.saveJWT([
+        [jwtsService.TYPES.ACCESS, accessToken],
+        [jwtsService.TYPES.REFRESH, refreshToken]
+    ]);
     return {
         accessToken,
         refreshToken
@@ -60,25 +30,23 @@ export const createTokens = async (payload) => {
 }
 
 export const revokeTokens = async (token) => {
-    return revokeJWT(token)
+    return jwtsService.revokeJWT(token)
 }
 
 export const verifyAccessToken = async (token, cb) => {
-    const isAvailable = await isAvailableJWT(token);
-
-    if (!isAvailable) {
-        cb(new Error('Token does not exist'), null);
-    } else {
+    const isAvailable = await jwtsService.isAvailableJWT(jwtsService.TYPES.ACCESS, token);
+    if (isAvailable) {
         jwt.verify(token, ACCESS_TOKEN_SECRET, cb);
+    } else {
+        cb(new Error('Token does not exist'), null);
     }
 }
 
 export const verifyRefreshToken = async (token, cb) => {
-    const isAvailable = await isAvailableJWT(token);
-
-    if (!isAvailable) {
-        cb(new Error('Token does not exist'), null);
-    } else {
+    const isAvailable = await jwtsService.isAvailableJWT(jwtsService.TYPES.REFRESH, token);
+    if (isAvailable) {
         jwt.verify(token, REFRESH_TOKEN_SECRET, cb);
+    } else {
+        cb(new Error('Token does not exist'), null);
     }
 }
